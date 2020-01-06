1272bed1b2a26cd70a5beee930b22199c47be732
Tiago Valdrich <tiago.valdrich@camerite.com>
2020-01-06 19:46:17 -0300

    Raising error instead of return it

--- Tool.py
+++ Tool.py
@@ -2,10 +2,10 @@
 class Tool:
 
     def draw(self, position):
-        return NotImplementedError("The subclass must implement this method.")
+        raise NotImplementedError("The subclass must implement this method.")
 
     def is_over(self, position):
-        return NotImplementedError("The subclass must implement this method.")
+        raise NotImplementedError("The subclass must implement this method.")
 
     def do_functionality(self):
-        return NotImplementedError("The subclass must implement this method.")
+        raise NotImplementedError("The subclass must implement this method.")
